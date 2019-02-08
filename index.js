5bde8037a29c5a20fcbf6fc47a0af6b874ed96fe
Adrian Henry <adrian@hungryturtlecode.com>
2019-02-08 15:09:48 +0000

    changed visualisation of the game to be a historical heatmap

--- index.js
+++ index.js
@@ -8,10 +8,21 @@ canvas.height = 800;
 const COLS = canvas.width / resolution;
 const ROWS = canvas.height / resolution;
 
+class Cell {
+  constructor() {
+    this.currentstate = Math.floor(Math.random() * 2);
+    this.total = 0;
+  }
+  setState(state) {
+    this.currentstate = state;
+    this.total += state;
+  }
+}
+
 function buildGrid() {
   return new Array(COLS).fill(null)
     .map(() => new Array(ROWS).fill(null)
-      .map(() => Math.floor(Math.random() * 2)));
+      .map(() => new Cell()));
 }
 
 let grid = buildGrid();
@@ -25,11 +36,12 @@ function update() {
 }
 
 function nextGen(grid) {
-  const nextGen = grid.map(arr => [...arr]);
+  // const nextGen = grid.map(arr => [...arr]);
+  const currentGen = grid.map(arr => arr.map(cell => cell.currentstate));
 
-  for (let col = 0; col < grid.length; col++) {
-    for (let row = 0; row < grid[col].length; row++) {
-      const cell = grid[col][row];
+  for (let col = 0; col < currentGen.length; col++) {
+    for (let row = 0; row < currentGen[col].length; row++) {
+      const cell = currentGen[col][row];
       let numNeighbours = 0;
       for (let i = -1; i < 2; i++) {
         for (let j = -1; j < 2; j++) {
@@ -40,7 +52,7 @@ function nextGen(grid) {
           const y_cell = row + j;
 
           if (x_cell >= 0 && y_cell >= 0 && x_cell < COLS && y_cell < ROWS) {
-            const currentNeighbour = grid[col + i][row + j];
+            const currentNeighbour = currentGen[col + i][row + j];
             numNeighbours += currentNeighbour;
           }
         }
@@ -48,25 +60,39 @@ function nextGen(grid) {
 
       // rules
       if (cell === 1 && numNeighbours < 2) {
-        nextGen[col][row] = 0;
+        grid[col][row].setState(0);
       } else if (cell === 1 && numNeighbours > 3) {
-        nextGen[col][row] = 0;
+        grid[col][row].setState(0);
       } else if (cell === 0 && numNeighbours === 3) {
-        nextGen[col][row] = 1;
+        grid[col][row].setState(1);
+      } else {
+        grid[col][row].setState(grid[col][row].currentstate);
       }
     }
   }
-  return nextGen;
+  return grid;
 }
 
 function render(grid) {
+  let maxTotal = 0;
+  for (let col = 0; col < grid.length; col++) {
+    for (let row = 0; row < grid[col].length; row++) {
+      const cell = grid[col][row];
+      if (cell.total > maxTotal) {
+        maxTotal = cell.total;
+      }
+    }
+  }
+
   for (let col = 0; col < grid.length; col++) {
     for (let row = 0; row < grid[col].length; row++) {
       const cell = grid[col][row];
 
       ctx.beginPath();
       ctx.rect(col * resolution, row * resolution, resolution, resolution);
-      ctx.fillStyle = cell ? 'black' : 'white';
+      const normalised = cell.total / maxTotal;
+      const h = (1.0 - normalised) * 240
+      ctx.fillStyle = `hsl(${h}, 100%, 50%)`;
       ctx.fill();
       // ctx.stroke();
     }
